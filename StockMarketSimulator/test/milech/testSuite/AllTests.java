685071c8069f164c2656e34a218d5c09bd284a00
Mikolaj Lech <mikolajj.lech@gmail.com>
2015-08-10 15:28:34 +0200

    Add CustomerTest to test suite.

--- StockMarketSimulator/test/milech/testSuite/AllTests.java
+++ StockMarketSimulator/test/milech/testSuite/AllTests.java
@@ -1,4 +1,5 @@
 package milech.testSuite;
+import milech.customer.CustomerTest;
 import milech.repository.StockMarketTest;
 import milech.service.BrokerageOfficeTest;
 
@@ -7,7 +8,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ StockMarketTest.class, BrokerageOfficeTest.class })
+@SuiteClasses({ StockMarketTest.class, BrokerageOfficeTest.class, CustomerTest.class })
 public class AllTests {
 
 }
