43b633ed7fb23596360cbc3a7742e5e541444619
Mikolaj Lech <mikolajj.lech@gmail.com>
2015-08-13 09:40:20 +0200

    Add map stock market implementation.

---
+++ StockMarketSimulator/src/milech/repository/StockMarketMapImpl.java
@@ -0,0 +1,122 @@
+package milech.repository;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+import milech.entity.Stock;
+import milech.reader.CsvReader;
+import milech.reader.Reader;
+
+public class StockMarketMapImpl implements StockMarket {
+
+//	private Map<Date, List<Stock>> stocks = new HashMap<Date, List<Stock>>();
+	private Map<Date, List<Stock>> stocks = new TreeMap<Date, List<Stock>>(); // already sorted
+//	private Set days;
+//	private Iterator iterator;
+	private LinkedList<Date> days;
+	private ListIterator iterator;
+	private Reader csvReader;
+	
+	public StockMarketMapImpl(String dataSource) {
+		csvReader = new CsvReader(dataSource);
+		loadAllData();
+		days = new LinkedList<Date>(stocks.keySet());
+		iterator = days.listIterator();
+	}
+	
+	public void loadAllData() {
+		while(loadNextStock() != null) {}
+	}
+	
+	public List<Stock> getNextDay() {
+		if(iterator.hasNext()) {
+			List<Stock> day = stocks.get(iterator.next());
+			return day;
+		}
+		return null;
+	}
+	
+	public List<Stock> getCurrentDay() {
+		List<Stock> day = stocks.get(iterator.previous());
+		return day;
+	}
+	
+	
+	public StockMarketMapImpl() {}
+	
+	public boolean equals(StockMarket compStockMarket) {
+		if(this.getDaysNum() != compStockMarket.getDaysNum())
+			return false;
+		for(int i = 0; i < this.getDaysNum(); i++) {
+//			if(!this.stocks.get(i).equals(compStockMarket.getStock(i))) {
+//				return false;
+//			}
+		}
+		return true;
+	}
+	
+	public int getDaysNum() {
+		return stocks.size();
+	}
+	
+	public Stock loadNextStock() {
+		Stock nextStock = getNextStock();
+		return add(nextStock);
+	}
+	
+	private Stock getNextStock() {
+		String line = "";
+		String csvSplitBy = ",";
+		Stock newStock = new Stock();
+		if((line = csvReader.getNextLine()) != null) {
+			// use comma as separator
+			String[] stockData = line.split(csvSplitBy);
+			newStock.setName(stockData[0]);
+			newStock.setDate(stockData[1]);
+			newStock.setPrice(stockData[2]);
+			return newStock;
+		}
+		return null;
+	}
+
+	public void setDataSource(String dataSource) {
+		csvReader = new CsvReader(dataSource);
+	}
+
+	public Stock add(Stock stock) {
+		if(stock == null) {
+			return null;
+		}
+		if(stocks.get(stock.getDate()) == null) {
+			List<Stock> newDay = new ArrayList<Stock>();
+			newDay.add(stock);
+			stocks.put(stock.getDate(), newDay);
+		}
+		else {
+			List<Stock> stockListToPut = stocks.get(stock.getDate());
+			stockListToPut.add(stock);
+			stocks.put(stock.getDate(), stockListToPut);
+		}
+		return stock;
+	}
+	
+	public String toString() {
+		String outStr = "";
+		for(Date marketDay : stocks.keySet()) {
+			for(Stock stock : stocks.get(marketDay)) {
+				outStr += stock.toString();
+			}
+		}
+		return outStr;
+	}
+	
+
+}
