8201cdeefeba240ea6ad7034f08de2a6db306f03
Mikolaj Lech <mikolajj.lech@gmail.com>
2015-08-21 13:47:05 +0200

    Add test for getStockTillToday();
    Signed-off-by: Mikolaj Lech <mikolajj.lech@gmail.com>

--- StockMarketSimulator/test/milech/repository/StockMarketTest.java
+++ StockMarketSimulator/test/milech/repository/StockMarketTest.java
@@ -3,9 +3,13 @@ package milech.repository;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import milech.entity.Stock;
+import milech.parser.Parser;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -72,11 +76,28 @@ public class StockMarketTest {
 	}
 	
 	@Test
-	public void shouldGetStocksTillToday() {
+	public void shouldGet2StocksTillToday() {
+		Map<Date, List<Stock>> testMap = new TreeMap<Date, List<Stock>>();
+		List<Stock> testDay = new ArrayList<Stock>();
+		testDay.add(new Stock("PKOBP", "20130102", "37.35"));
+		testDay.add(new Stock("KGHM", "20130102", "193.1"));
+		testDay.add(new Stock("PGNIG", "20130102", "5.26"));
+		testDay.add(new Stock("JSW", "20130102", "94.6"));
+		testDay.add(new Stock("TPSA", "20130102", "12.16"));
+		testMap.put(Parser.parseDate("20130102"), testDay);
+		
+		testDay = new ArrayList<Stock>();
+		testDay.add(new Stock("PKOBP", "20130103", "37.35"));
+		testDay.add(new Stock("KGHM", "20130103", "193.5"));
+		testDay.add(new Stock("PGNIG", "20130103", "5.25"));
+		testDay.add(new Stock("JSW", "20130103", "96.4"));
+		testDay.add(new Stock("TPSA", "20130103", "12.13"));
+		testMap.put(Parser.parseDate("20130103"), testDay);
+		
 		stockMarket.moveXDaysForward(2);
-		System.out.println(stockMarket.toString());
-		System.out.println("size: " + stockMarket.getStockMarketTillToday().getStockSize() 
-				+ " till today: \n" + stockMarket.getStockMarketTillToday().toString());
+		
+		assertEquals(new StockMarketMapImpl(testMap).toString(), 
+				stockMarket.getStockMarketTillToday().toString());
 	}
 	
 }
